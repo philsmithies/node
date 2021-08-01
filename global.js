2ba08140f073b82143e8a26e0545d0a14c162702
Phil Smithies <philsmithies@gmail.com>
2021-08-01 14:27:28 +0200

    global object

---
+++ global.js
@@ -0,0 +1,22 @@
+// Global Object
+
+// console.log(global)
+
+// global is the main window object like you have in js
+
+
+
+// global objects that are availiable to us
+// setTimeout(() => {
+//   console.log('in the timeout')
+// }, 3000);
+
+// const int = setInterval(() => {
+//   console.log('in the interval')
+// }, 1000)
+
+
+// find the directory path and the filepath 
+
+console.log(__dirname)
+console.log(__filename)
---
+++ index.js
@@ -0,0 +1,10 @@
+// const name = 'yoshi'
+
+// console.log(name)
+
+const greet = (name) => {
+  console.log(`hello, ${name}`)
+}
+
+greet('Mario')
+greet('Yoshi')
