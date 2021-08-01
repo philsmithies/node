73536100fcb1ad2c2410007b67bcc31dbdbfe238
Phil Smithies <philsmithies@gmail.com>
2021-08-01 23:53:26 +0200

    middleware done

---
+++ 06 - middleware/app.js
@@ -0,0 +1,50 @@
+const express = require('express')
+const morgan = require('morgan')
+
+// express app
+const app = express()
+
+// register view engine 
+app.set('view engine', 'ejs')
+
+// listen for requests
+app.listen(3000)
+
+// middleware and static files place files in the public folder and you can 
+// access it
+app.use(express.static('public'))
+app.use(morgan('dev'))
+
+// requests
+app.get('/', (req, res) => {
+  // handles our requests and the response codes for us
+  const blogs = [
+    {title: 'Yoshi finds eggs', snippet: 'Lorem impsum'},
+    {title: 'Luigi finds mario', snippet: 'Lorem impsum'},
+    {title: 'Bowser finds peach', snippet: 'Lorem impsum'},
+  ]
+  res.render('index', { title: 'Home', blogs })
+})
+
+
+app.get('/about', (req, res) => {
+  res.render('about', { title: 'About' })
+})
+
+// redirects
+app.get('/about-us', (req, res) => {
+  res.redirect('./about')
+})
+
+
+app.get('/blogs/create', (req, res) => {
+  res.render('create', { title: 'Create a new blog' })
+})
+
+// 404 page
+
+// it fires for anything that doesn't reach a match in the code above, so we need 
+// to keep this at the bottom
+app.use((req, res) => {
+  res.status(404).render('404', { title: '404' })
+})
